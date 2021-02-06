1ee2106a758e7f051f9b7f5ff44dfb1738234cf0
Tobias Nett <skaldarnar@googlemail.com>
2021-02-06 18:34:56 +0100

    feat(JOML): migrate `BlockComponent` (#28)

--- src/main/java/org/terasology/signalling/blockFamily/SignalCableBlockFamily.java
+++ src/main/java/org/terasology/signalling/blockFamily/SignalCableBlockFamily.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.signalling.blockFamily;
 
 import org.joml.Vector3i;
@@ -20,7 +7,6 @@ import org.joml.Vector3ic;
 import org.terasology.blockNetwork.BlockNetworkUtil;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.prefab.Prefab;
-import org.terasology.math.JomlUtil;
 import org.terasology.math.Rotation;
 import org.terasology.math.Side;
 import org.terasology.math.SideBitFlag;
@@ -114,7 +100,7 @@ public class SignalCableBlockFamily extends MultiConnectFamily {
     private boolean connectsToNeighbor(Side connectSide, boolean input, boolean output, EntityRef neighborEntity) {
         final Side oppositeDirection = connectSide.reverse();
 
-        Block block = neighborEntity.getComponent(BlockComponent.class).getBlock();
+        Block block = neighborEntity.getComponent(BlockComponent.class).block;
 
         final SignalConductorComponent neighborConductorComponent = neighborEntity.getComponent(SignalConductorComponent.class);
         if (neighborConductorComponent != null) {
--- src/main/java/org/terasology/signalling/componentSystem/SignalSwitchBehaviourSystem.java
+++ src/main/java/org/terasology/signalling/componentSystem/SignalSwitchBehaviourSystem.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.signalling.componentSystem;
 
 import com.google.common.collect.Maps;
@@ -316,7 +303,7 @@ public class SignalSwitchBehaviourSystem extends BaseComponentSystem implements 
     private boolean processOutputForSetResetGate(EntityRef blockEntity) {
         SignalGateComponent signalGateComponent = blockEntity.getComponent(SignalGateComponent.class);
         SignalConsumerAdvancedStatusComponent consumerAdvancedStatusComponent = blockEntity.getComponent(SignalConsumerAdvancedStatusComponent.class);
-        Block block = blockEntity.getComponent(BlockComponent.class).getBlock();
+        Block block = blockEntity.getComponent(BlockComponent.class).block;
         Side resetSide = signalGateComponent.functionalSides.get(0);
         Integer resetSignal = consumerAdvancedStatusComponent.signalStrengths.get(BlockNetworkUtil.getResultSide(block, resetSide).name());
 
